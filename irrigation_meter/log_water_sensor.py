ec30c50639582edc55c5f1acd2f4d789d301a5dd
Niv Gal Waizer <nivwiz@gmail.com>
2016-06-09 00:30:49 +0300

    count litters by the sensor limits

--- irrigation_meter/log_water_sensor.py
+++ irrigation_meter/log_water_sensor.py
@@ -4,38 +4,54 @@ from socket import socket
 import os
 import select
 
-#taken from irrigation-bone/wfs_config.py
+"""
+This sensor: http://www.seeedstudio.com/wiki/G3/4_Water_Flow_sensor
+Shows a coeff of 5.5 between pulse frequecy and flow rate.
+when the flow rate is  2L/min it issues pulses at 11Hz
+When the flow rate is 10L/min it issues pulses at 55Hz
+sensor range is 1-60 L/min, so the frequency range is 5.5-330Hz
+converting min -> sec:
+range is 0.01667-1 L/sec
+"""
+
 gpio_base = '/sys/class/gpio'
 gpio = '48' #P9_15
 WATERFLOW_TIMEOUT = 60 #no water flow for a minute
+DEBOUNCE_DELAY = 1/5.5  #sec
 
 gpio_path = os.path.join( gpio_base, 'gpio' + gpio )
 gpio_value = os.path.join(gpio_path, 'value')
 
 def main():
-  bounches_sum = bounches_during_sec = 0
+  triggers_during_sec = last_litter_timestamp = 0
+  previous_timestamp = timestamp = int( time.time() )
   sock = socket()
   sock.connect( ('127.0.0.1', 2003) )
-  previous_timestamp = timestamp = int( time.time() )
   with open(gpio_value) as gpio_fd:
     po = select.epoll()
     po.register(gpio_fd, select.EPOLLIN | select.EPOLLET)  
     while True:
       events = po.poll( WATERFLOW_TIMEOUT )
-      timestamp = int( time.time() )
+      timestamp = time.time()
       gpio_fd.seek(0)
       value = gpio_fd.read().strip()
-      if previous_timestamp == timestamp:
-        bounches_during_sec +=1
-        message = 'water.flow.bounces {} {}\n' .format(bounches_during_sec, timestamp)
+      if value == '1' :
+        if (timestamp - last_litter_timestamp) > DEBOUNCE_DELAY:
+          if int(last_litter_timestamp) != int(timestamp):
+            current_litter = 1/5.5
+          else:
+            current_litter += 1/5.5
+          message = 'water.flow.count {} {}\n' .format(current_litter, int(timestamp))
+          sock.sendall(message)
+          last_litter_timestamp = timestamp
+
+        if int(previous_timestamp) == int(timestamp):
+          triggers_during_sec += 1/5.5
+        else:
+          triggers_during_sec = 1/5.5
+        message = 'water.flow.triggers {} {}\n' .format(triggers_during_sec, int(timestamp) )
         sock.sendall(message)
-      else:
-        bounches_sum += bounches_during_sec
-        bounches_during_sec = 0
-      message = 'water.flow.count {} {}\n' .format(bounches_sum, timestamp)
-      #print message
-      sock.sendall(message)
-      previous_timestamp = timestamp
+        previous_timestamp = timestamp
     po.unregister(gpio_fd)
   sock.close()
 
